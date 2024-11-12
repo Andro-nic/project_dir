35c0ccb69eb53e1afb31dd50ed0df9c967cfe176
Andrey <kv.andrey@gmail.com>
2024-11-12 22:09:44 +0300

    changed news_portal and templates

---
+++ project/news_portal/templatetags/custom_filters.py
@@ -0,0 +1,16 @@
+from django import template
+from .bad_words import BED_WORDS
+
+register = template.Library()
+
+
+@register.filter()
+def sensor(text, profanity=BED_WORDS):
+    if not isinstance(text, str):  # проверяем является ли переменная text строкой(если нет выдаем исключение)
+        raise TypeError('Переменная к которой применяется фильтр должна быть строкового типа')
+    for word in text.split():
+        if word.lower() in profanity:
+            new_word = word[0] + '.' * (len(word)-1)
+            text = text.replace(word, new_word)
+
+    return text
